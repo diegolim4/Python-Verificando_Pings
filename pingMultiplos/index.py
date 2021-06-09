98e905d1d22e2d630d77d0729773b04f9801fdc8
Suelen Almeida <lima.j.diego@gmail.com>
2021-06-09 12:32:14 -0300

    Usando a biblioteca time entre um ping e outro

--- pingMultiplos/index.py
+++ pingMultiplos/index.py
@@ -1,9 +1,11 @@
 import os
+import time
 
 with open('hosts.txt') as file:
     file = file.read()
     file = file.splitlines()
 
     for ip in file:
-        os.system('ping ' + ip)
-
+        os.system(f'ping {ip}')
+        print('_' * 50)
+        time.sleep(5)
